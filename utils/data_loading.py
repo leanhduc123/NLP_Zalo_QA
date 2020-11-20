817b4c29e5856c6457f14f81989ba01e8fc8c98e
10 <leeanhducc@gmail.com>
2020-11-20 21:30:20 +0700

    'update'

--- utils/data_loading.py
+++ utils/data_loading.py
@@ -3,10 +3,13 @@ import pandas as pd
 class ZaloLoader:
     
     def __init__(self):
-        self.data = []
+        self.data = pd.DataFrame(data={})
 
     def read_csv(self, filepath):
         self.data = self.data.append(pd.read_csv(filepath))
+        return self.data
+    def read_json(self, filepath):
+        self.data = self.data.append(pd.read_json(filepath))
         return self.data
 
 
